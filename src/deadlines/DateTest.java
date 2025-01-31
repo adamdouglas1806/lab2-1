d247a9353e5b73da9410c69032f388cb8cf14506
adamd <adamd@TABLET-H8PMISRM.Home>
2025-01-31 10:20:02 +0000

    I have completed lab 1 program

--- src/deadlines/DateTest.java
+++ src/deadlines/DateTest.java
@@ -12,11 +12,13 @@ class DateTest {
 		assertEquals(date.getDay(), 5, "The output is supposed to be 5");
 	}
 	
+	@Test
 	void testGetDayOfYear() {
 		Date date = new Date(3, 1);
 		assertEquals(date.getDayOfYear(), 3, "The outpust is supposed to be 3");
 	}
 	
+	@Test
 	void testToString() {
 		Date date = new Date(6, 4);
 		assertEquals("6/4/2025", date.ToString());
