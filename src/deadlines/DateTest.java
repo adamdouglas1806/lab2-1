3905a1873d6ec8bcdd16a2e7c9bf151e30c3a708
adamd <adamd@TABLET-H8PMISRM.Home>
2025-01-31 10:08:37 +0000

    I have updated the DateTest class

--- src/deadlines/DateTest.java
+++ src/deadlines/DateTest.java
@@ -7,8 +7,19 @@ import org.junit.jupiter.api.Test;
 class DateTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testGetDay() {
+		Date date = new Date(5, 2);
+		assertEquals(date.getDay(), 5, "The output is supposed to be 5");
+	}
+	
+	void testGetDayOfYear() {
+		Date date = new Date(3, 1);
+		assertEquals(date.getDayOfYear(), 3, "The outpust is supposed to be 3");
+	}
+	
+	void testToString() {
+		Date date = new Date(6, 4);
+		assertEquals("6/4/2025", date.ToString());
 	}
 
 }
